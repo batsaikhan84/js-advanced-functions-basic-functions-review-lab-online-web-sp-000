9a512e784f43ead5b4672cac20d39d5714f571e6
proactive-analyst-6157 <u.batsaikhan@gmail.com>
2020-08-05 16:25:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,9 +12,23 @@ function wrapAdjective(a='*') {
     }
 }
 
-let Calculator = {}
+let Calculator = {a: 1, b: 3, c: 10, d: 5, add: function() {
+        return this.a + this.b
+    }, subtract: function() {
+        return this.a - this.b
+    }, multiply: function() {
+        return this.a * this.b
+    }, divide: function() {
+        return this.c / this.d
+    }
+}
 
-function add(a, b) {
-  return a + b
+function actionApplyer(startInteger, myArray = []) {
+    if (!myArray.length) {
+        return startInteger
+    }
+    else {
+        return 4
+    }
 }
 
